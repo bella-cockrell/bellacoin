ab5a77126b8f3c459c5b154e2f6bb2dae721e5aa
bella-cockrell <arabella.cockrell@gmail.com>
2021-07-13 20:31:14 +0100

    Adding a few more test examples and refactor

--- spec/bellacoin_spec.rb
+++ spec/bellacoin_spec.rb
@@ -4,8 +4,16 @@ describe BellaCoin, "#hash" do
 
     context "when string is parsed" do
 
-        it "produces the correct hash" do
-            expect(described_class.hash("Hello, Cryptos!")).to eq("33eedea60b0662c66c289ceba71863a864cf84b00e10002ca1069bf58f9362d5")
+        hash_examples = {
+            "Hello, Cryptos!" => "33eedea60b0662c66c289ceba71863a864cf84b00e10002ca1069bf58f9362d5",
+            "Your Name Here" => "39459289c09c33a7b516bef926c1873c6ecd2e6db09218b065d7465b6736f801",
+            "Data Data Data Data" => "a7bbfc531b2ecf641b9abcd7ad8e50267e1c873e5a396d1919f504973090565a" 
+        }
+
+        hash_examples.each do |key, value|
+            it "the string '#{key}' produces the correct hash" do
+                expect(described_class.hash(key)).to eq(value)
+            end
         end
         
     end
